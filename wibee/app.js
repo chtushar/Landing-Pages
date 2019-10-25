b4e59a0919d86f3d56d552b746b113aa55c223d6
Tushar Choudhari <choudharit23@gmail.com>
2019-10-25 19:17:06 +0530

    id

--- wibee/app.js
+++ wibee/app.js
@@ -2,12 +2,12 @@ var menuButton = document.querySelector('.menu-button');
 
 function menuOpenClose() {
   if (menuButton.classList.contains('open')) {
-    TweenMax.to('.navigation', 1, {
+    TweenMax.to('#navigation', 1, {
       x: '100%',
       ease: Power2.easeInOut,
     });
   } else {
-    TweenMax.to('.navigation', 1, {
+    TweenMax.to('#navigation', 1, {
       x: '0%',
       ease: Power2.easeInOut,
     });

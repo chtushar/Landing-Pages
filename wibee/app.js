02ec948b2ab26941badba8bb6e32be2c352608ff
Tushar Choudhari <choudharit23@gmail.com>
2019-10-25 22:44:58 +0530

    animations added

--- wibee/app.js
+++ wibee/app.js
@@ -2,6 +2,20 @@ window.addEventListener('load', () => {
   var container = document.querySelector('.container');
 
   container.style.display = 'block';
+
+  TweenMax.to('.logo', 1, {
+    opacity: 1,
+    ease: Power2.easeInOut,
+  });
+  TweenMax.to('.reveal-statement', 2, {
+    opacity: 1,
+    y: '-50%',
+    ease: Circ.easeOut,
+  });
+  TweenMax.to('.statement', 2, {
+    opacity: 1,
+    ease: Circ.easeOut,
+  });
 });
 
 var menuButton = document.querySelector('.menu-button');

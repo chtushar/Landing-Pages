0bba584c35ca5c3676455015869f1d29d4115523
Tushar Choudhari <choudharit23@gmail.com>
2019-10-25 18:57:29 +0530

    project added

---
+++ wibee/app.js
@@ -0,0 +1,20 @@
+var menuButton = document.querySelector('.menu-button');
+
+function menuOpenClose() {
+  if (menuButton.classList.contains('open')) {
+    TweenMax.to('#navigation', 1, {
+      x: '100%',
+      ease: Power2.easeInOut,
+    });
+  } else {
+    TweenMax.to('#navigation', 1, {
+      x: '0%',
+      ease: Power2.easeInOut,
+    });
+  }
+  menuButton.classList.toggle('open');
+}
+
+menuButton.addEventListener('click', () => {
+  menuOpenClose();
+});
